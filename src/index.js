662ccad4d1420d7f8abfb7f61c4a57a7d7923c89
Rutthynhaa <ruth_ellen.araujo@hotmail.com>
2024-06-04 12:58:39 -0300

    Segundo Desafio Felipao

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function calculadoraRankeada(vitorias, derrotas) {
+    let saldoRankeadas = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Derrotado!";
+    }
+
+    console.log(`O Herói tem saldo de ${saldoRankeadas} e está no nível de ${nivel}`);
+}
+
+calculadoraRankeada(75, 20);
